f2f152b5c42a8ab5635b90efc8b6b7910d6ff361
antunezFernando <antunez.fernandoivan.43377@gmail.com>
2024-09-20 10:36:37 -0300

    Change event handling so that the squares will only be drawn on keeping the mouse down

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const mainContainer = document.querySelector("#squares-container");
+const squaresContainer = document.querySelector("#squares-container");
 
-createSquares(50)
+createSquares(16)
 
 function createSquares(num) {
     if(typeof num !== "number" || num > 100 || num < 1) {
@@ -13,15 +13,37 @@ function createSquares(num) {
         for(let j = 0; j < num; j++) {
             let squareDiv = document.createElement("div");
             squareDiv.classList.add("square");
-            squareDiv.addEventListener("mouseenter", (e) => {
-                changeColor(e.target, "black");
-            });
             rowDiv.appendChild(squareDiv);
         }
-        mainContainer.appendChild(rowDiv);
+        squaresContainer.appendChild(rowDiv);
+
+        let func = (event) => {
+            mouseOverEvent(event);
+        }
+
+        squaresContainer.addEventListener("mousedown", (event) => {
+            event.preventDefault();
+            mouseDownEvent(squaresContainer, func);
+        })
+
+        squaresContainer.addEventListener("mouseup", () => {
+            mouseUpEvent(squaresContainer, func);
+        })
     }
 }
 
 function changeColor(div, color) {
-    div.style.backgroundColor = color
+    div.style.backgroundColor = color;
+}
+
+function mouseDownEvent(element, func) {
+    element.addEventListener("mouseover", func);
+}
+
+function mouseOverEvent(event) {
+    changeColor(event.target, "black");
+}
+
+function mouseUpEvent(element, functionToRemove) {
+    element.removeEventListener("mouseover", functionToRemove);
 }
